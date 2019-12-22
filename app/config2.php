e30a6464f89e2f9df7b1d7f6678074b615bfdc52
carlosgendev <carlostopdev321@protonmail.com>
2019-12-22 20:54:55 +0900

    [update] updated dashboard style

--- app/config2.php
+++ app/config2.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'doniaweb');
+define('DB_USER', 'mysocialsignweb');
+define('DB_PASS', 'TqAah#IXY3$s');
+define('DB_NAME', 'mysocialsignweb');
 define('TIMEZONE', 'Pacific/Niue');
 define('ENCRYPTION_KEY', '8de0682e4b3633865bc7ccbcdc29defb');
