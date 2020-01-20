e17ee7a18bccdb349287d9ab7eaf2c8b64e07e9d
carlosgendev <carlostopdev321@protonmail.com>
2020-01-20 16:54:17 +0900

    [update] added title in account manager page

--- app/modules/account_manager/views/account/index.php
+++ app/modules/account_manager/views/account/index.php
@@ -1,5 +1,19 @@
-<div class="wrap-content">
-    <div class="row">
-        <?php load_account()?>  
-    </div>
+<div class="wrap-content">
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <h4 class="page-title float-left">Dashboard</h4>
+                <ol class="breadcrumb float-right">
+                    <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                </ol>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </div>
+    <div class="row m-t--30">
+
+        <?php load_account()?>  
+
+    </div>
+
 </div>
--- app/modules/dashboard/views/index.php
+++ app/modules/dashboard/views/index.php
@@ -21,11 +21,9 @@ $module_name = "dashboard";
         <div class="col-12">
             <div class="page-title-box">
                 <h4 class="page-title float-left">Dashboard</h4>
-
                 <ol class="breadcrumb float-right">
                     <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
                 </ol>
-
                 <div class="clearfix"></div>
             </div>
         </div>
